e3d24735b6262026436c072c58c869654cc849a3
Neha panbude <nehapanbude123@gmail.com>
2024-11-23 12:52:42 +0530

    first commit

---
+++ 3d-ARRAY/array.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){ 
+ int arr[2][2][3]={1,2,3,4,5,6,7,8,9,1,2,3};
+ for(int t=0; t<2; t++)
+ {
+    for (int r=0; r<2; r++)
+    {
+        for (int c=0; c<3; c++)
+        {
+            cout<<arr[t] [r] [c]<<"\t";
+
+        }
+        cout<<"\n";
+    }
+    cout<<"\n";
+ }
+}
